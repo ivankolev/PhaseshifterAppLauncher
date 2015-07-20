b97e02c64088729967c51032d13ffbbb006ff739
Ivan Kolev <ivan.kolev@mobilelive.ca>
2015-07-20 01:04:23 -0400

    Initial commit - My App Portfolio assignment

---
+++ first/src/main/java/com/phaseshiftlab/phaseshifterapp/first/MainActivity.java
@@ -0,0 +1,58 @@
+package com.phaseshiftlab.phaseshifterapp.first;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openToastMessage(View view){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        StringBuilder toastText = new StringBuilder();
+        toastText.append("This button will launch my ").append(((Button) view).getText()).append(" application");
+
+        Toast toast = Toast.makeText(context, toastText, duration);
+        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+        if( v != null) v.setGravity(Gravity.CENTER);
+        toast.show();
+    }
+}
